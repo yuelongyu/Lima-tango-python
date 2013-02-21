b15ae215d35b09290db528d7c94fffa2f2314f39
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2013-02-21 10:41:58 +0100

    add DevCcdWriteAll command

--- plugins/LimaTacoCCD.py
+++ plugins/LimaTacoCCD.py
@@ -776,6 +776,20 @@ class LimaTacoCCDs(PyTango.Device_4Impl, object):
         saving.writeFrame(frame_nb,1,synchronous)
 
 #------------------------------------------------------------------
+#    DevCcdWriteAll command:
+#
+#    Description: 
+#------------------------------------------------------------------
+    @Core.DEB_MEMBER_FUNCT
+    def DevCcdWriteAll(self):
+	control = _control_ref()
+	acq = control.acquisition()
+	nbConcat = acq.getConcatNbFrames()
+
+        saving = control.saving()
+        saving.writeFrame(0,nbConcat)
+
+#------------------------------------------------------------------
 #    DevCcdGetBin command:
 #
 #    Description: 
@@ -1130,6 +1144,9 @@ class LimaTacoCCDsClass(PyTango.DeviceClass):
             [PyTango.DevLong, ""]],
         'DevCcdWriteFile':
             [[PyTango.DevLong, "frame to write"],
+            [PyTango.DevVoid, ""]],
+	'DevCcdWriteAll':
+            [[PyTango.DevVoid, ""],
             [PyTango.DevVoid, ""]],
         'DevCcdGetBin':
             [[PyTango.DevVoid, ""],
