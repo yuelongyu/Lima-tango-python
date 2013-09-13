ffc8ac6ff42d60ceaa8e4e1fed3f68ea355add8d
bliss administrator <blissadm@lid243.esrf.fr>
2013-09-13 10:35:27 +0200

    improve plug-in for XH detector (Beamline test) (Geoff Mant + MCD)

--- camera/Xh.py
+++ camera/Xh.py
@@ -64,6 +64,11 @@ class Xh(PyTango.Device_4Impl):
         PyTango.Device_4Impl.__init__(self,*args)
 
         self.init_device()
+	
+	self.__clockmode = {'XhInternalClock': 0,
+			    'XhESRF5468Mhz': 1,
+			    'XhESRF1136Mhz': 2}
+			    
 
 #------------------------------------------------------------------
 #    Device destructor
@@ -95,17 +100,114 @@ class Xh(PyTango.Device_4Impl):
 
         return valueList
 
+    def __getDictKey(self,dict, value):
+        try:
+            ind = dict.values().index(value)                            
+        except ValueError:
+            return None
+        return dict.keys()[ind]
 
+    def __getDictValue(self,dict, key):
+        try:
+            value = dict[key]
+        except KeyError:
+            return None
+        return value
+
+
+#------------------------------------------------------------------
+#    reset command:
+#
+#    Description: make a full reset: 
+#    - call of init() method which run the config.cmd file
+#      defined in the properties 
+#    argout: DevVoid   
+#------------------------------------------------------------------
+    @Core.DEB_MEMBER_FUNCT
+    def reset(self):
+        _XhCam.reset()
+		
 #==================================================================
 #
-#    Andor read/write attribute methods
+#    setHeadCaps command
+#
+#==================================================================
+    @Core.DEB_MEMBER_FUNCT
+    def setHeadCaps(self,argin):
+    	l = len(argin)
+    	capsAB = argin[0]
+	capsCD = argin[1]
+	
+	print l
+	print capsAB
+	print capsCD
+	
+	
+    	_XhCam.setHeadCaps(capsAB,capsCD)
+		
+#==================================================================
+#
+#    setCommand command
+#
+#==================================================================
+    @Core.DEB_MEMBER_FUNCT
+    def sendCommand(self,argin):
+    	cmd = argin
+	
+	print cmd
+	
+    	_XhCam.sendCommand(cmd)
+		
+#==================================================================
+#
+#    Xh read/write attribute methods
 #
 #==================================================================
 
 
     def __getattr__(self,name) :
         return get_attr_4u(self, name, _XhInterface)
+	
+	
 
+#------------------------------------------------------------------
+#    write clockmode:
+#
+#    Description: writes the clockMode
+#    argin: DevString   
+#------------------------------------------------------------------	
+
+    def write_clockmode(self,attr):
+	data = attr.get_write_value()
+	print data
+	clockmode = self.__getDictValue(self.__clockmode,data)
+	print clockmode
+	_XhCam.setupClock(clockmode)
+
+#------------------------------------------------------------------
+#    write nbscans:
+#
+#    Description: writes the nbscans 
+#    argin: DevInt   
+#------------------------------------------------------------------	
+
+    def write_nbscans(self,attr):
+	data = attr.get_write_value()
+	nbscans = data
+	print nbscans
+	_XhCam.setNbScans(nbscans)
+
+
+#------------------------------------------------------------------
+#    read nbscans:
+#
+#    Description: reads the nbscans 
+#    argin: DevInt   
+#------------------------------------------------------------------	
+
+    def read_nbscans(self,attr):
+	nbscans = _XhCam.getNbScans()
+	attr.set_value(nbscans)
 
 
 #------------------------------------------------------------------
@@ -134,14 +236,32 @@ class XhClass(PyTango.DeviceClass):
         'getAttrStringValueList':
         [[PyTango.DevString, "Attribute name"],
          [PyTango.DevVarStringArray, "Authorized String value list"]],
+        'reset':
+        [[PyTango.DevVoid, ""],
+         [PyTango.DevVoid, ""]],
+        'setHeadCaps':
+        [[PyTango.DevVarULongArray, "Caps for AB, Caps for CD"],
+         [PyTango.DevVoid, ""]],
+        'sendCommand':
+        [[PyTango.DevString, "da.server command"],
+         [PyTango.DevVoid, ""]],
         }
-
+		
     attr_list = {
+       	'clockmode':
+	[[PyTango.DevString,
+	PyTango.SCALAR,
+	PyTango.WRITE]],
+       	'nbscans':
+	[[PyTango.DevLong,
+	PyTango.SCALAR,
+	PyTango.READ_WRITE]],
         }
 
     def __init__(self,name) :
         PyTango.DeviceClass.__init__(self,name)
         self.set_type(name)
+
 
 #------------------------------------------------------------------
 #------------------------------------------------------------------
@@ -159,6 +279,7 @@ def get_control(cam_ip_address = "0",port = 1972,config_name = 'config',**keys) 
         print cam_ip_address
 	print port
 	print config_name
+#	Core.DebParams.setTypeFlags(Core.DebParams.AllFlags)
 	_XhCam = XhAcq.Camera(cam_ip_address,int(port),config_name)
 	_XhInterface = XhAcq.Interface(_XhCam)
     return Core.CtControl(_XhInterface)
