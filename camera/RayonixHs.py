16592903e2011b0e75b97daec98483a883b1cb85
Laurent Claustre <claustre@esrf.fr>
2014-03-21 15:40:42 +0100

    * added new attributes for signal type/id and temperature control

--- camera/RayonixHs.py
+++ camera/RayonixHs.py
@@ -43,15 +43,11 @@ import PyTango
 import sys, types, os, time
 
 from Lima import Core
-from Lima import RayonixHs as RayonixHsModule
 # import some useful helpers to create direct mapping between tango attributes
 # and Lima interfaces.
 from AttrHelper import get_attr_4u, get_attr_string_value_list
+import AttrHelper
 
-class RayonixHs(PyTango.Device_4Impl):
-
-    Core.DEB_CLASS(Core.DebModApplication, 'LimaCCDs')
-    
 #==================================================================
 #   RayonixHs Class Description:
 #
@@ -69,20 +65,63 @@ class RayonixHs(PyTango.Device_4Impl):
     @Core.DEB_MEMBER_FUNCT
     def __init__(self,cl, name):
         PyTango.Device_4Impl.__init__(self,cl,name)
-        self.init_device()
+
         # dictionnaries to be used with AttrHelper.get_attr_4u
         self.__FrameMode = {'SINGLE':RayonixHsAcq.SINGLE,
                            'FAST_TRANSFER':RayonixHsAcq.FAST_TRANSFER}
-        self.__TriggerSignalType = {'OPTO': RayonixHsAcq.OPTO,
+
+        self.__SignalType = {'OPTO': RayonixHsAcq.OPTO,
                                     'OPTO_INVERTED': RayonixHsAcq.OPTO_INVERTED,
                                     'CMOS': RayonixHsAcq.CMOS,
                                     'CMOS_PULLDOWN': RayonixHsAcq.CMOS_PULLDOWN,
                                     'CMOS_PULLUP': RayonixHsAcq.CMOS_PULLUP,
                                     'CMOS_PULLDOWN_INVERTED': RayonixHsAcq.CMOS_PULLDOWN_INVERTED,
                                     'CMOS_PULLUP_INVERTED': RayonixHsAcq.CMOS_PULLUP_INVERTED}
+        self.__OutputSignalID = {'SHUTTER': RayonixHsAcq.ID_SHUTTER,
+                           'INTEGRATE': RayonixHsAcq.ID_INTEGRATE,
+                           'FRAME': RayonixHsAcq.ID_FRAME,
+                           'LINE': RayonixHsAcq.ID_LINE,
+                           'SHUTTER_OPENING': RayonixHsAcq.ID_SHUTTER_OPENING,
+                           'SHUTTER_CLOSING': RayonixHsAcq.ID_SHUTTER_CLOSING,
+                           'SHUTTER_ACTIVE': RayonixHsAcq.ID_SHUTTER_ACTIVE,
+                           'TRIGGER_RISE_WAIT': RayonixHsAcq.ID_TRIGGER_RISE_WAIT,
+                           'TRIGGER_RISE_ACK': RayonixHsAcq.ID_TRIGGER_RISE_ACK,
+                           'TRIGGER_FALL_WAIT': RayonixHsAcq.ID_TRIGGER_FALL_WAIT,
+                           'TRIGGER_FALL_ACK': RayonixHsAcq.ID_TRIGGER_FALL_ACK,                           
+                           'TRIGGER_2_RISE_WAIT': RayonixHsAcq.ID_TRIGGER_2_RISE_WAIT,
+                           'TRIGGER_2_RISE_ACK': RayonixHsAcq.ID_TRIGGER_2_RISE_ACK,
+                           'INPUT_FRAME': RayonixHsAcq.ID_INPUT_FRAME,
+                           'INPUT_GATE': RayonixHsAcq.ID_INPUT_GATE,
+                           }
+        self___OutputChannel = {'CHAN1': RayonixHsAcq.CHANNEL_1,
+                             'CHAN2': RayonixHsAcq.CHANNEL_2
+                             }
+        self.__Cooler = {'START': True,
+                         'STOP': False}
+        self.__VacuumValve = {'OPEN': True,
+                              'CLOSE': False}
+        self.__ElectronicShutterEnabled = {'TRUE': True,
+                                           'FALSE': False}
+
+        self.__FrameTriggerSignalType = self.__SignalType
+        self.__SequenceGateSignalType = self.__SignalType
+        self.__OutputSignalType = self.__SignalType
         self.__Attribute2FunctionBase = {'frame_mode': 'FrameMode',
-                                         'trigger_signal_type': 'TriggerSignalType',
+                                         'frame_trigger_signal_type': 'FrameTriggerSignalType',
+                                         'sequence_gate_signal_type': 'SequenceGateSignalType',
+                                         'electronic_shutter_enabled': 'ElectronicShutterEnabled',
+                                         'cooler_temperature_setpoint': 'CoolerTemperatureSetpoint',
+                                         'sensor_temperature_setpoint': 'SensorTemperatureSetpoint',
+                                         'cooler': 'Cooler',
+                                         'vacuum_valve': 'VacuumValve',
                                          }
+        self.__OtherAttribute2FunctionBase = {'output1_signal_type': 'OutputSignalType',
+                                              'output2_signal_type': 'OutputSignalType',
+                                              'output1_id': 'OutputSignalID',
+                                              'output2_id': 'OutputSignalID'
+                                              }
+        self.init_device()
+
                                                
 #------------------------------------------------------------------
 #    Device destructor
@@ -102,12 +141,62 @@ class RayonixHs(PyTango.Device_4Impl):
         self.get_device_properties(self.get_device_class())
 
         # Apply properties if any
-        #if self.p_gain:
-        #    _RayonixHsInterface.setPGain(self.p_gain)
+        if self.frame_mode:
+            self.applyNewPropery('frame_mode')
+        
+        if self.frame_trigger_signal_type:
+            self.applyNewPropery('frame_trigger_signal_type')
+
+        if self.sequence_gate_signal_type:
+            self.applyNewPropery('sequence_gate_signal_type')
+        
+        if self.electronic_shutter_enabled:
+            self.applyNewPropery('electronic_shutter_enabled')
+
+        if self.output1_signal_type:
+            self.applyNewPropery('electronic_shutter_enabled')
+
+        if self.electronic_shutter_enabled:
+            self.applyNewPropery('electronic_shutter_enabled')
+
+        if self.cooler_temperature_setpoint:
+            _RayonixHsInterface.setCoolerTemperatureSetpoint(self.cooler_temperature_setpoint)
+
+        if self.sensor_temperature_setpoint:
+            _RayonixHsInterface.setSensorTemperatureSetpoint(self.sensor_temperature_setpoint)
+
+        if self.output1_signal_type:
+            self.applyNewPropery('output1_signal_type', RayonixHsAcq.CHANNEL_1)
+
+        if self.output2_signal_type:
+            self.applyNewPropery('output2_signal_type', RayonixHsAcq.CHANNEL_2)
+
+        if self.output1_id:
+            self.applyNewPropery('output1_id', RayonixHsAcq.CHANNEL_1)
+
+        if self.output1_id:
+            self.applyNewPropery('output1_id', RayonixHsAcq.CHANNEL_2)
+
+                
+    @Core.DEB_MEMBER_FUNCT
+    def applyNewPropery(self, prop_name, extra=None):
+        if extra is not None: name = self.__OtherAttribute2FunctionBase[prop_name]
+        else: name = self.__Attribute2FunctionBase[prop_name]
+                                           
+        dict = getattr(self, '_'+self.__class__.__name__+'__'+name)
+        func = getattr(_RayonixHsInterface, 'set'+name)
+        key = getattr(self, prop_name)
+        val = AttrHelper._getDictValue(dict, key.upper())
+        if  val is None:
+            deb.Error('Wrong value for property %s :%s' % (prop_name, val))
+        else:
+            if extra is not None: func(extra,val)
+            else: func(val)
+
             
             
-        #if self.shutter_level:
-        #    _RayonixHsInterface.setShutterLevel(self.__ShutterLevel[self.shutter_level])
+           
+            
 
 #==================================================================
 #
@@ -120,15 +209,70 @@ class RayonixHs(PyTango.Device_4Impl):
         return get_attr_4u(self, name, _RayonixHsInterface)
 
 
-    ## @brief return the timing times, exposure and latency
+    ## @brief return the ID on output channel #1
     #  
-    def read_timing(self, attr):
-        timing=[]
-        timing.append(_RayonixHsCamera.getExpTime())
-        timing.append(_RayonixHsCamera.getLatTime())
+    @Core.DEB_MEMBER_FUNCT
+    def read_output1_id(self, attr):        
+        id = _RayonixHsInterface.getOutputSignalID(RayonixHsAcq.CHANNEL_1)
+        val = AttrHelper._getDictKey(self.__OutputSignalID, id)
+        attr.set_value(val)        
         
-        attr.set_value(timing,2)        
+    ## @brief write the ID on output channel #1
+    #  
+    @Core.DEB_MEMBER_FUNCT
+    def write_output1_id(self, attr):
+        id = attr.get_write_value()
+        val = AttrHelper._getDictValue(self.__OutputSignalID, id)
+        _RayonixHsInterface.setOutputSignalID(RayonixHsAcq.CHANNEL_1, val)
+
+    ## @brief return the ID on output channel #2
+    #  
+    @Core.DEB_MEMBER_FUNCT
+    def read_output2_id(self, attr):        
+        id = _RayonixHsInterface.getOutputSignalID(RayonixHsAcq.CHANNEL_2)
+        val = AttrHelper._getDictKey(self.__OutputSignalID, id)
+        attr.set_value(val)        
         
+    ## @brief write the ID on output channel #2
+    #  
+    @Core.DEB_MEMBER_FUNCT
+    def write_output2_id(self, attr):
+        id = attr.get_write_value()
+        val = AttrHelper._getDictValue(self.__OutputSignalID, id)
+        _RayonixHsInterface.setOutputSignalID(RayonixHsAcq.CHANNEL_2, val)
+
+    ## @brief return the signal type on output channel #1
+    #  
+    @Core.DEB_MEMBER_FUNCT
+    def read_output1_signal_type(self, attr):        
+        sig_type = _RayonixHsInterface.getOutputSignalType(RayonixHsAcq.CHANNEL_1)
+        val = AttrHelper._getDictKey(self.__OutputSignalType, sig_type)
+        attr.set_value(val)        
+        
+    ## @brief write the signal type  on output channel #1
+    #  
+    @Core.DEB_MEMBER_FUNCT
+    def write_output1_signal_type(self, attr):
+        sig_type = attr.get_write_value()
+        val = AttrHelper._getDictValue(self.__OutputSignalType, sig_type)
+        _RayonixHsInterface.setOutputSignalType(RayonixHsAcq.CHANNEL_1, val)
+
+    ## @brief return the signal type on output channel #2
+    #  
+    @Core.DEB_MEMBER_FUNCT
+    def read_output2_signal_type(self, attr):        
+        sig_type = _RayonixHsInterface.getOutputSignalType(RayonixHsAcq.CHANNEL_2)
+        val = AttrHelper._getDictKey(self.__OutputSignalType, sig_type)
+        attr.set_value(val)        
+        
+    ## @brief write the signal type  on output channel #2
+    #  
+    @Core.DEB_MEMBER_FUNCT
+    def write_output2_signal_type(self, attr):
+        sig_type = attr.get_write_value()
+        val = AttrHelper._getDictValue(self.__OutputSignalType, sig_type)
+        _RayonixHsInterface.setOutputSignalType(RayonixHsAcq.CHANNEL_2, val)
+
 
 #==================================================================
 #
@@ -144,7 +288,12 @@ class RayonixHs(PyTango.Device_4Impl):
 #------------------------------------------------------------------
     @Core.DEB_MEMBER_FUNCT
     def getAttrStringValueList(self, attr_name):
-        return get_attr_string_value_list(self, attr_name)
+        if attr_name in ['output1_id','output2_id']:
+            return self.__OutputSignalID.keys()
+        elif attr_name in ['output1_signal_type', 'output2_signal_type']:
+            return self.__OutputSignalType.keys()
+        else:
+            return get_attr_string_value_list(self, attr_name)
     
 
 #==================================================================
@@ -160,11 +309,37 @@ class RayonixHsClass(PyTango.DeviceClass):
 
     #    Device Properties
     device_property_list = {
-        'config_path':
+        'frame_mode':
         [PyTango.DevString,
-         'configuration path directory', []],
+         'Frame Transfer mode', ['single']],
+        'frame_trigger_signal_type':
+        [PyTango.DevString,
+         'Frame trigger (input 1) signal type', ['opto']],
+        'sequence_gate_signal_type':
+        [PyTango.DevString,
+         'Sequence gate (input 2) signal type', ['opto']],
+        'electronic_shutter_enabled':
+        [PyTango.DevString,
+         'Electronic shutter enabled', ['false']],
+        'cooler_temperature_setpoint':
+        [PyTango.DevString,
+         'Detector cooling setpoint', [-120]],
+        'sensor_temperature_setpoint':
+        [PyTango.DevString,
+         'Sensor temperature setpoint', [-80]],
+        'output1_signal_type':
+        [PyTango.DevString,
+         'Output channel #1 signal type', ['opto']],
+        'output2_signal_type':
+        [PyTango.DevString,
+         'Output channel #2 signal type', ['opto']],
+        'output1_id':
+        [PyTango.DevString,
+         'Output channel #1 ID', ['shutter']],
+        'output2_id':
+        [PyTango.DevString,
+         'Output channel #2 ID', ['frame']],
         }
-
 
     #    Command definitions
     cmd_list = {
@@ -181,18 +356,99 @@ class RayonixHsClass(PyTango.DeviceClass):
           PyTango.SCALAR,
           PyTango.READ_WRITE],
          {
-             'label':'To switch from std to fast-transfer mode',
+             'label':'Frame Transfer mode',
              'unit': 'SINGLE/FAST_TRANSFER',
              }],
-        'trigger_signal_type':
+        'frame_trigger_signal_type':
         [[PyTango.DevString,
           PyTango.SCALAR,
           PyTango.READ_WRITE],
          {
-             'label':'Set frame trigger input signal type',
+             'label':'Frame trigger (input 1) signal type',
              'unit': 'OPTO/OPTO_INVERTED/CMOS/CMOS_PULLDOWN/CMOS_PULLUP/CMOS_PULLDOWN_INVERTED/CMOS_PULLUP_INVERTED',
              }],
                 
+        'sequence_gate_signal_type':
+        [[PyTango.DevString,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE],
+         {
+             'label':'Sequence gate (input 2) signal type',
+             'unit': 'OPTO/OPTO_INVERTED/CMOS/CMOS_PULLDOWN/CMOS_PULLUP/CMOS_PULLDOWN_INVERTED/CMOS_PULLUP_INVERTED',
+             }],
+                
+        'electronic_shutter_enabled':
+        [[PyTango.DevString,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE],
+         {
+             'label':'Electronic shutter enabled',
+             'unit': 'TRUE/FALSE',
+             }],                
+        'cooler_temperature_setpoint':
+        [[PyTango.DevDouble,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE],
+         {
+             'label':'Detector cooling setpoint',
+             'unit': 'deg. Celcius',
+             }],                
+        'sensor_temperature_setpoint':
+        [[PyTango.DevDouble,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE],
+         {
+             'label':'Sensor temperature setpoint',
+             'unit': 'deg. Celcius',
+             }],
+        'cooler':
+        [[PyTango.DevString,
+          PyTango.SCALAR,
+          PyTango.WRITE],
+         {
+             'label':'Detector cooling',
+             'unit': 'START/STOP',
+             }],
+        'vacuum_valve':
+        [[PyTango.DevString,
+          PyTango.SCALAR,
+          PyTango.WRITE],
+         {
+             'label':'Vacuum valve',
+             'unit': 'OPEN/CLOSE',
+             }],
+        'output1_signal_type':
+        [[PyTango.DevString,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE],
+         {
+             'label':'Output channel #2 signal type',
+             'unit': 'OPTO/OPTO_INVERTED/CMOS/CMOS_PULLDOWN/CMOS_PULLUP/CMOS_PULLDOWN_INVERTED/CMOS_PULLUP_INVERTED',
+             }],
+        'output2_signal_type':
+            [[PyTango.DevString,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE],
+         {
+             'label':'Output channel #2 signal type',
+             'unit': 'OPTO/OPTO_INVERTED/CMOS/CMOS_PULLDOWN/CMOS_PULLUP/CMOS_PULLDOWN_INVERTED/CMOS_PULLUP_INVERTED',
+             }],
+        'output1_id':
+        [[PyTango.DevString,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE],
+         {
+             'label':'Output channel #1 ID',
+             'unit': 'SHUTTER/INTEGRATE/FRAME/LINE/SHUTTER_OPENING/SHUTTER_CLOSING/SHUTTER_ACTIVE/TRIGGER_RISE_WAIT/TRIGGER_RISE_ACK/TRIGGER_FALL_WAIT/TRIGGER_FALL_ACK/TRIGGER_2_RISE_WAIT/TRIGGER_2_RISE_ACK/INPUT_FRAME/INPUT_GATE',
+             }],
+        'output2_id':
+        [[PyTango.DevString,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE],
+         {
+             'label':'Output channel #2 ID',
+             'unit': 'SHUTTER/INTEGRATE/FRAME/LINE/SHUTTER_OPENING/SHUTTER_CLOSING/SHUTTER_ACTIVE/TRIGGER_RISE_WAIT/TRIGGER_RISE_ACK/TRIGGER_FALL_WAIT/TRIGGER_FALL_ACK/TRIGGER_2_RISE_WAIT/TRIGGER_2_RISE_ACK/INPUT_FRAME/INPUT_GATE',
+             }],
         }
 
 #------------------------------------------------------------------
