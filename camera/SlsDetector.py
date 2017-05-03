1ecbbf1770c18ef111f6861bffc90cbf15c86f3c
Alejandro Homs Puron <ahoms@esrf.fr>
2017-05-03 14:41:52 +0200

    SlsDetector: add generic DAC & ADC control

--- camera/SlsDetector.py
+++ camera/SlsDetector.py
@@ -53,12 +53,43 @@ class SlsDetector(PyTango.Device_4Impl):
 #------------------------------------------------------------------
 #    Device constructor
 #------------------------------------------------------------------
+
     def __init__(self,*args) :
         PyTango.Device_4Impl.__init__(self,*args)
         self.init_device()
 
         self.cam = _SlsDetectorCam
         self.model = self.cam.getModel()
+
+        nb_modules = self.cam.getNbDetSubModules()
+        name_list, dac_idx_list = self.model.getDACInfo()
+        attr_name_list = map(lambda n: 'dac_' + n, name_list)
+        self.dac_attr_idx_list = zip(attr_name_list, dac_idx_list)
+        for name, index in self.dac_attr_idx_list:
+            attr_data_dict = {
+                'name': name, 
+                'dtype': PyTango.DevLong,
+                'dformat': PyTango.SPECTRUM,
+                'max_dim_x': nb_modules,
+                'fget': self.read_dac,
+                'fset': self.write_dac,
+            }
+            attr_data = PyTango.AttrData.from_dict(attr_data_dict)
+            self.add_attribute(attr_data)
+
+        name_list, adc_idx_list = self.model.getADCInfo()
+        attr_name_list = map(lambda n: 'adc_' + n, name_list)
+        self.adc_attr_idx_list = zip(attr_name_list, adc_idx_list)
+        for name, index in self.adc_attr_idx_list:
+            attr_data_dict = {
+                'name': name, 
+                'dtype': PyTango.DevLong,
+                'dformat': PyTango.SPECTRUM,
+                'max_dim_x': nb_modules,
+                'fget': self.read_adc,
+            }
+            attr_data = PyTango.AttrData.from_dict(attr_data_dict)
+            self.add_attribute(attr_data)
 
 #------------------------------------------------------------------
 #    Device destructor
@@ -117,16 +148,48 @@ class SlsDetector(PyTango.Device_4Impl):
         return resp
 
     @Core.DEB_MEMBER_FUNCT
-    def read_high_voltage(self, attr):
-        hvolt = self.cam.getHighVoltage()
-        deb.Return("hvolt=%s" % hvolt)
-        attr.set_value(hvolt)
+    def read_dac_name_list(self, attr):
+        dac_name_list = zip(*self.dac_attr_idx_list)[0]
+        deb.Return("dac_name_list=%s" % (dac_name_list,))
+        attr.set_value(dac_name_list)
 
     @Core.DEB_MEMBER_FUNCT
-    def write_high_voltage(self, attr):
-        hvolt = attr.get_write_value()
-        deb.Param("hvolt=%s" % hvolt)
-        self.cam.setHighVoltage(hvolt)
+    def read_dac(self, attr):
+        idx = dict(self.dac_attr_idx_list)[attr.get_name()]
+        dac_list = self.cam.getDACList(idx)
+        deb.Return("dac_list=%s" % dac_list)
+        attr.set_value(dac_list)
+
+    @Core.DEB_MEMBER_FUNCT
+    def write_dac(self, attr):
+        idx = dict(self.dac_attr_idx_list)[attr.get_name()]
+        dac_list = attr.get_write_value()
+        deb.Param("dac_list=%s" % dac_list)
+        nb_dac = len(dac_list)
+        if nb_dac == 1:
+            dac_idx_list = [-1]
+        elif nb_dac == self.cam.getNbDetSubModules():
+            dac_idx_list = range(nb_dac)
+        else:
+            msg = 'Invalid %s length: %s' % (attr.get_name(), dac_list)
+            deb.Error(msg)
+            raise ValueError(msg)
+        for i, dac_val in zip(dac_idx_list, dac_list):
+            if dac_val >= 0:
+                self.cam.setDAC(i, idx, dac_val)
+
+    @Core.DEB_MEMBER_FUNCT
+    def read_adc_name_list(self, attr):
+        adc_name_list = zip(*self.adc_attr_idx_list)[0]
+        deb.Return("adc_name_list=%s" % (adc_name_list,))
+        attr.set_value(adc_name_list)
+
+    @Core.DEB_MEMBER_FUNCT
+    def read_adc(self, attr):
+        idx = dict(self.adc_attr_idx_list)[attr.get_name()]
+        adc_list = self.cam.getADCList(idx)
+        deb.Return("adc_list=%s" % adc_list)
+        attr.set_value(adc_list)
 
     @Core.DEB_MEMBER_FUNCT
     def read_threshold_energy(self, attr):
@@ -172,10 +235,14 @@ class SlsDetectorClass(PyTango.DeviceClass):
         [[PyTango.DevString,
           PyTango.SPECTRUM,
           PyTango.READ, 64]],
-        'high_voltage':
-        [[PyTango.DevLong,
-          PyTango.SCALAR,
-          PyTango.READ_WRITE]],
+        'dac_name_list':
+        [[PyTango.DevString,
+          PyTango.SPECTRUM,
+          PyTango.READ, 64]],
+        'adc_name_list':
+        [[PyTango.DevString,
+          PyTango.SPECTRUM,
+          PyTango.READ, 64]],
         'raw_mode':
         [[PyTango.DevBoolean,
           PyTango.SCALAR,
