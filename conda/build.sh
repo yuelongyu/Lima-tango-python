11d0ac2c5dd0888e339a4269c4b0693e9937f0af
Samuel Debionne <samuel.debionne@esrf.fr>
2018-09-18 15:09:45 +0200

    Fix conda recipe build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -1,4 +1,4 @@
 cp scripts/LimaCCDs $PREFIX/bin
 mkdir -p $SP_DIR/Lima/Server && cp *.py $SP_DIR/Lima/Server/
 mkdir -p $SP_DIR/Lima/Server/camera && cp camera/__init__.py $SP_DIR/Lima/Server/camera/
-mkdir -p $SP_DIR/Lima/Server/plugins && cp -R plugins $SP_DIR/Lima/Server/plugins/
+mkdir -p $SP_DIR/Lima/Server/plugins && cp -R plugins $SP_DIR/Lima/Server/
