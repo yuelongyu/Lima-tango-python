03204b988852589b3a38e94305b67a5209bbc26a
Laurent Claustre <claustre@lid00limace.esrf.fr>
2019-01-29 16:12:27 +0100

    py3 again

--- LimaViewer.py
+++ LimaViewer.py
@@ -254,9 +254,9 @@ def main():
         U.server_init()
         U.server_run()
 
-    except PyTango.DevFailed(e):
+    except PyTango.DevFailed as e:
         print('-------> Received a DevFailed exception:',e)
-    except Exception(e):
+    except Exception as e:
         print('-------> An unforeseen exception occured....',e)
 
 if __name__ == '__main__':
